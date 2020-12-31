b5ed71a5fb7bd46e5315b7580b0fc0fcf7e66efb
Faisal Ali <dev.faisalali@gmail.com>
2020-12-31 12:02:36 -0600

    LRS: Removed seed and orm submodules.

--- orator/__init__.py
+++ orator/__init__.py
@@ -2,7 +2,7 @@
 
 __version__ = "0.9.9"
 
-from .orm import Model, SoftDeletes, Collection, accessor, mutator, scope
+#from .orm import Model, SoftDeletes, Collection, accessor, mutator, scope
 from .database_manager import DatabaseManager
 from .query.expression import QueryExpression
 from .schema import Schema
